aa94f4438a3ac1d1aa3a7f1431de5d995f60579d
Joris Guffens <joris.guffens@outlook.com>
2022-11-26 17:01:12 +0100

    removed unused stuff

--- common/src/main/java/com/guflimc/brick/stats/common/BrickStatsConfig.java
+++ common/src/main/java/com/guflimc/brick/stats/common/BrickStatsConfig.java
@@ -2,27 +2,8 @@ package com.guflimc.brick.stats.common;
 
 import com.guflimc.brick.orm.ebean.database.EbeanConfig;
 
-import java.util.List;
-
 public class BrickStatsConfig {
 
     public EbeanConfig database;
-
-    public int powerGiftAmount;
-    public int powerGiftInterval;
-
-    public String clanChatPrefix;
-    public String clanNametagPrefix;
-    public String clanRegionNone;
-
-    public SidebarTemplate attackSidebar;
-    public int attackDuration; // minutes
-
-    public static class SidebarTemplate {
-
-        public String title;
-        public List<String> lines;
-
-    }
 
 }
